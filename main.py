851c535fb742172a6ea33ec80d2a530b90a34170
Prajay Singh Silwal <prajaysingh@hotmail.com>
2020-08-21 00:00:23 -0400

    Update in comment section of main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+#This program prints the 2nd smallest and 2nd largest item in the list
 num = [4,7,3,2,2,8,8,5,5,8]
 newlist =[]
 leng = len(num)
 flag = False
 for i in range(leng):
     for j in range (i+1,leng):
-        if ((num[i]==num[j]) and (i==leng-2) and (j==leng-1)): #This condition is to test if the 2nd last item and last item is equal. This will avoid to miss appending of last item
+        if ((num[i]==num[j]) and (i==leng-2) and (j==leng-1)): #This condition is to test if the 2nd last item and last item are equal. This will avoid missing appending of last item
             flag = True
             newlist.append(num[i])
             break
@@ -17,7 +18,7 @@ for i in range(leng):
             
     if (flag == False):
         newlist.append(num[i])
-print(newlist)
+print(newlist) #Check to see if duplicate items are being removed in new list
 newlist.sort()
 print(newlist)
 len2= len(newlist)
